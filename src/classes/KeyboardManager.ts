dab137611f30fce981e2980c64dab52840313006
marnixah <marnix@hagehoeve.com>
2022-01-24 10:39:11 +0100

    feat!: Special keys support(!?# etc)

--- src/classes/KeyboardManager.ts
+++ src/classes/KeyboardManager.ts
@@ -5,11 +5,26 @@ export default class KeyboardManager {
   col: number;
   tv: TV;
   modifiers: string[];
+  currentKeyboardIndex = 0;
   keyboard = [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ','],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', 'Backspace'],
-    ['Special', 'Left', 'Right', ' ', ' ', ' ', '-', '_', 'Enter', 'Enter'],
+    [
+      ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+      ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ','],
+      ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', 'Backspace'],
+      ['Key1', 'Left', 'Right', ' ', ' ', ' ', '-', '_', 'Enter', 'Enter'],
+    ],
+    [
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+      ['@', '#', '$', '_', '&', '-', '+', '(', ')', '/'],
+      ['Key2', '*', '"', "'", ':', ';', '!', '?', '%', 'Backspace'],
+      ['Key0', 'Left', 'Right', ' ', ' ', ' ', '.', ',', 'Enter', 'Enter'],
+    ],
+    [
+      ['~', '`', '|', '•', '√', 'π', '÷', '×', '¶', '∆'],
+      ['£', '¢', '€', '¥', '^', '°', '=', '{', '}', '\\'],
+      ['Key1', '%', '©', '®', '✓', '[', ']', '<', '>', 'Backspace'],
+      ['Key0', 'Left', 'Right', ' ', ' ', ' ', '.', ',', 'Enter', 'Enter'],
+    ],
   ];
 
   constructor(tv: TV) {
@@ -33,32 +48,40 @@ export default class KeyboardManager {
   }
 
   async sendKey(key: string): Promise<void> {
-    const [row, col] = this.keyToRowCol(key);
-    if (key.toUpperCase() === key) await this.shift(); // if the letter is capital, shift
+    const [row, col, keyboard] = this.keyToRowCol(key);
+    await this.setKeyboard(keyboard);
+    if (key.toUpperCase() === key && keyboard === 0) await this.shift(); // if the letter is capital, shift
     await this.setCol(col); // horizontal first
     await this.setRow(row); // then vertical
     await this.tv.ok();
     if (row === 3) await this.setRow(2); // special keys are annoying to navigate, so we go back to the previous row
-    if (key === ' ') this.col--; // I do not know why, but the space key is sending the column -1. Thanks sony TV
   }
 
-  keyToRowCol(key: string): [number, number] {
+  keyToRowCol(key: string): [number, number, number] {
     // Special cases first
-    if (key === ' ') return [3, 3];
+    if (key === ' ') return [3, 3, this.currentKeyboardIndex];
+    if (key === 'Backspace') return [2, 9, this.currentKeyboardIndex];
+    if (key === 'Enter') return [3, 8, this.currentKeyboardIndex];
+    if (key === 'Left') return [3, 1, this.currentKeyboardIndex];
+    if (key === 'Right') return [3, 2, this.currentKeyboardIndex];
+    if (key === 'Key0') return [3, 0, this.currentKeyboardIndex];
+    if (key === 'Shift') return [2, 0, this.currentKeyboardIndex];
 
-    let row = 0;
-    let col = 0;
-    for (let i = 0; i < this.keyboard.length; i++) {
-      const rowKeys = this.keyboard[i];
-      for (let j = 0; j < rowKeys.length; j++) {
-        if (rowKeys[j].toLowerCase() === key.toLowerCase()) {
-          row = i;
-          col = j;
-          break;
+    // Then the regular keys
+    let [row, col, keyboard] = [0, 0, 0];
+    findLoop: for (const [keyI, curKeyboard] of this.keyboard.entries()) {
+      for (const [rowI, curRow] of curKeyboard.entries()) {
+        for (const [colI, curCol] of curRow.entries()) {
+          if (curCol.toLowerCase() === key.toLowerCase()) {
+            row = rowI;
+            col = colI;
+            keyboard = keyI;
+            break findLoop;
+          }
         }
       }
     }
-    return [row, col];
+    return [row, col, keyboard];
   }
   async setRow(row: number): Promise<void> {
     const diff = row - this.row;
@@ -78,6 +101,12 @@ export default class KeyboardManager {
       await this.tv.left(-diff);
     }
     this.col = col;
+  }
+
+  async setKeyboard(index: number): Promise<void> {
+    if (index === this.currentKeyboardIndex) return;
+    await this.sendKey(`Key${index}`);
+    this.currentKeyboardIndex = index;
   }
 
   async shift(): Promise<void> {
